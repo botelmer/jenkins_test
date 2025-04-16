79dbeb36fd6ac6059b26d0b73c42d8d89cf62ef5
Elmer Mamani <elmer.mamani.urrelo@gmail.com>
2025-04-15 20:38:23 -0400

    initial commit

---
+++ src/main/java/jenkins_test/App.java
@@ -0,0 +1,12 @@
+package jenkins_test;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo jenkins!!");
+	}
+
+	public static int sumar(int a, int b) {
+		return a + b;
+	}
+}
---
+++ src/test/java/jenkins_test/AppTest.java
@@ -0,0 +1,12 @@
+package jenkins_test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AppTest {
+	@Test
+	public void testSuma() {
+		assertEquals(5, App.sumar(2, 3));
+	}
+}
